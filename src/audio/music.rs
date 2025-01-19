f27525d5e6733bf89a5b7fc7f94256343d7ba84a
ivankorchmit <criperadmiral92@gmail.com>
2025-01-19 04:54:45 +0200

    fmt

--- src/audio/music.rs
+++ src/audio/music.rs
@@ -3,8 +3,11 @@ use bevy_rustysynth::MidiAudio;
 
 pub fn setup_background_music(mut commands: Commands, asset_server: Res<AssetServer>) {
     let midi_handle = asset_server.load::<MidiAudio>("music/catacombs.mid");
-    commands.spawn((AudioPlayer(midi_handle), PlaybackSettings {
-        mode: bevy::audio::PlaybackMode::Loop,
-        ..default()
-    }));
+    commands.spawn((
+        AudioPlayer(midi_handle),
+        PlaybackSettings {
+            mode: bevy::audio::PlaybackMode::Loop,
+            ..default()
+        },
+    ));
 }
