78c6fbcfe6b2218de17090e2b610e013d6fc51cc
ivankorchmit <criperadmiral92@gmail.com>
2025-01-20 23:11:05 +0200

    add impl pick_location fn

--- src/gen/location.rs
+++ src/gen/location.rs
@@ -1,4 +1,20 @@
-use bevy::{ecs::system::Resource, math::IVec2, utils::HashSet};
+use bevy::{
+    ecs::system::Resource,
+    math::IVec2,
+    prelude::{Deref, DerefMut},
+    utils::HashSet,
+};
+use rand::prelude::*;
 
-#[derive(Resource, Default)]
-pub struct WorldCatacomb(pub HashSet<IVec2>);
+#[derive(Resource, Default, Deref, DerefMut)]
+pub struct WorldCatacomb(HashSet<IVec2>);
+
+impl WorldCatacomb {
+    pub fn pick_location(&self) -> IVec2 {
+        **self
+            .iter()
+            .collect::<Vec<_>>()
+            .choose(&mut thread_rng())
+            .unwrap()
+    }
+}
